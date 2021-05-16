1e5976802cc5bccd88c4f33d30e85ed241e99ca5
harshj5246 <jainh9712@gmail.com>
2021-05-17 00:24:18 +0530

    create menu item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,10 +3,7 @@ import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
-    <div className='homepage' 
-    style={{
-        backgroundImage: `url("https://i.ibb.co/cvpntL1/hats.png"))`
-    }}>
+    <div className='homepage' >
         <Directory />
     </div>
 );
